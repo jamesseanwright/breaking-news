60f936488b5b60caf277c8623f8e776a0458097b
Sky News Products Team <skynews.products@gmail.com>
2015-07-13 15:01:24 +0100

    rendering list child item components

---
+++ src/components/news-item.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class NewsItem extends React.Component {
+	render() {
+		return (
+			<div className="news-item">
+				<h2>{this.props.title}</h2>
+				<p>{this.props.author}</p>
+				<p>{this.props.children}</p>
+			</div>
+		);
+	}
+}
+
+export default NewsItem;
--- src/components/news-list.jsx
+++ src/components/news-list.jsx
@@ -1,8 +1,19 @@
-var React = require('react');
+import React from 'react';
+import NewsItem from './news-item.jsx';
 
 class NewsList extends React.Component {
 	render() {
-		return <div>I'm a NewsList</div>;
+		return (
+			<div className="items">
+				<NewsItem title="Title" author="Bob">
+					Some text for da news item lol
+				</NewsItem>
+
+				<NewsItem title="Title 2" author="Greg">
+					Some text for da news item lol
+				</NewsItem>
+			</div>
+		);
 	}
 }
 

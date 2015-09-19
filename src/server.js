515fbffdc693f7e0e94316a03810d9e48fc2f929
James Wright <james@jamesswright.co.uk>
2015-09-19 11:27:49 +0100

    Wrote skeleton for tutorial

--- src/server.js
+++ src/server.js
@@ -1,13 +1,7 @@
 import express from 'express';
 import swig from 'swig';
-import React from 'react';
-import NewsList from './components/news-list.jsx';
-import NewsStore from './stores/news-store';
-import NewsActions from './actions/news-actions';
-import iso from './iso';
 
 const port = process.env.PORT || 3000;
-const newsListFactory = React.createFactory(NewsList);
 const app = express();
 
 app.engine('swig', swig.renderFile);
@@ -17,20 +11,8 @@ app.set('view cache', false);
 swig.setDefaults({ cache: false });
 app.use(express.static(__dirname + '/public'));
 
-iso.add(new NewsStore());
-
 app.get('/', (req, res) => {
-	res.status(200).render('view.swig', { stories: React.renderToStaticMarkup(newsListFactory()), appState: iso.serialise() });
+	res.status(200).render('view.swig');
 }).listen(port);
-
-NewsActions.listenToFirebase({
-	type: 'once',
-	name: 'value'
-});
-
-NewsActions.listenToFirebase({
-	type: 'on',
-	name: 'child_added'
-});
 
 console.log('Listening on port', port, '...');
